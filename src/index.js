1423cc85b56a1c973804b3b7597bfcfe6201d977
Justin Traille <jet007@localhost.localdomain>
2020-09-03 21:28:12 -0400

    Fixed Warnings

--- src/index.js
+++ src/index.js
@@ -2,8 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import aws_config from './aws-exports';
+import { Auth } from 'aws-amplify';
+import { ApolloProvider } from 'react-apollo'
+import { Rehydrated } from 'aws-appsync-react' 
 
+
+const client = new AWSAppSyncClient({
+        url: aws_config.aws_appsync_graphqlEndpoint,
+        region: aws_config.aws_appsync_region,
+        auth: {
+                type: AUTH_TYPE.API_KEY,
+                jwtToken: async () => (await Auth.currentSession()).getAcceessToken().getJwtToken(),
+        }
+})
 
 ReactDOM.render(
-        <App />
+        <ApolloProvider client={client}>
+                <Rehydrated >
+                        <App/>
+                </Rehydrated>
+        </ApolloProvider>
+        
 , document.getElementById('root'));
